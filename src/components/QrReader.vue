e527bbf01affe70c9d48319c5d64a130210871db
soybean15 <marlonpadilla1593@gmail.com>
2023-11-20 17:38:56 +0800

    initial commit

---
+++ src/components/QrReader.vue
@@ -0,0 +1,40 @@
+<template>
+ <div class="stream">
+  <qr-stream @decode="onDecode" class="mb">
+    <div style="color: red;" class="frame"></div>
+  </qr-stream>
+</div>
+<div class="result">
+  Result: {{data}}
+</div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+import { QrStream, QrCapture, QrDropzone } from 'vue3-qr-reader';
+
+export default {
+    components:{
+        QrStream,
+    QrCapture,
+    QrDropzone
+},
+setup() {
+    const state = reactive({
+      data: null
+    })
+    function onDecode(data) {
+      state.data = data
+    }
+    return {
+      ...toRefs(state),
+      onDecode
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
