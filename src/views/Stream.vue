3770d0a7e637672f55c57d6dde867313dcad20da
soybean15 <marlonpadilla1593@gmail.com>
2023-11-20 18:25:42 +0800

    added qr:

--- src/views/Stream.vue
+++ src/views/Stream.vue
@@ -1,10 +1,17 @@
 <template>
-  <qr-stream @decode="onDecode" class="mb">
-    <div style="color: red;" class="frame"></div>
-  </qr-stream>
-<div class="result">
-  Result: {{data}}
-</div>
+  <div>
+    <div class="mb">
+      Qr Stream
+    </div>
+    <div class="center stream">
+      <qr-stream @decode="onDecode" class="mb">
+        <div style="color: red;" class="frame"></div>
+      </qr-stream>
+    </div>
+    <div class="result">
+      Result: {{data}}
+    </div>
+  </div>
 </template>
     
     <script>
@@ -20,7 +27,7 @@ export default {
     const state = reactive({
       data: null
     })
-    function onDecode(data) {
+    const  onDecode=(data) =>{
       state.data = data
     }
     return {
@@ -31,3 +38,23 @@ export default {
 };
 </script>
     
+<style scoped>
+.stream {
+  max-height: 500px;
+  max-width: 500px;
+  margin: auto;
+}
+.frame {
+  border-style: solid;
+  border-width: 2px;
+  border-color: red;
+  height: 200px;
+  width: 200px;
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  right: 0px;
+  left: 0px;
+  margin: auto;
+}
+</style>
