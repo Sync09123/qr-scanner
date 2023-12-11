1facfff8ff3e535ec6abb025b88e3da2fdf68487
sync09169 <abesamiscyrus71@gmail.com>
2023-12-11 14:14:22 +0800

    Automated deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to your Laravel project directory
-cd /home/soybean15/vue-projects/q-scanner/
+cd /home/russ/capstoneproject/qr-scanner
 
 # Add all changes to the staging area
 git add .
